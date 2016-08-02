d6e4b75a1fced33b078fef221728cd55863ca1a0
Eccenux <eccenux+git@gmail.com>
2016-08-02 17:10:28 +0200

    update test

--- userscripts/pgodb_filtering.user.js
+++ userscripts/pgodb_filtering.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        PokeGO DB filtering
 // @namespace   enux.pl
-// @description Filtering feature for long tables on PokeGO DB site. Works best on Pokedex and Overal move rankings. Also makes the title shorter (better for many tabs opened).
+// @description Filtering feature for long tables on PokeGO DB site. Works best on Pokedex and Overal move rankings (DPS). Also makes the title shorter (better for many tabs opened).
 // @include     http://www.pokemongodb.net/*
-// @version     1.0.1
+// @version     1.0.2
 // @grant       none
 // @updateURL   https://github.com/Eccenux/pgo-pgodb/raw/master/userscripts/pgodb_filtering.meta.js
 // @downloadURL https://github.com/Eccenux/pgo-pgodb/raw/master/userscripts/pgodb_filtering.user.js
